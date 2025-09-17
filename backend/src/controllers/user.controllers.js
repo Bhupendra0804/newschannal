14db7f8007773f59040c87672774c0b335f58e5b
Bhupendra1449 <sameer.bhupi@gmail.com>
2025-09-17 16:03:01 +0530

    commit first

---
+++ backend/src/controllers/user.controllers.js
@@ -0,0 +1,15 @@
+import { asyncHandler } from "../utils/asyncHandler";
+
+
+
+const userRegistration = asyncHandler(async(req, res)=>{
+    // check ulplicate user 
+    // fatch all data form UI
+    // check user existing
+    // saver user profile
+    
+
+})
+
+
+export {userRegistration}
