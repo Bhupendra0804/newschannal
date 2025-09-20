9786fce15890fd7a8e5c893eef3ea0474918cbb9
Bhupendra1449 <sameer.bhupi@gmail.com>
2025-09-20 23:26:39 +0530

    add register user

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -1,50 +1,58 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import {ApiError} from "../utils/ApiError.js"
+import { ApiError } from "../utils/ApiError.js"
 import { User } from "../models/user.model.js";
 import { uploadPhotoOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
+const userRegistration = asyncHandler(async (req, res) => {
+    const { email, username, fullname, password } = req.body; //get user Details
 
+    if ([fullname, email, username, password].some((field) => field?.trim() === "")) {  //user details not empty
+        throw new ApiError(400, "All filed fill mendetory")
+    }
 
+    const userExist = await User.findOne({      //check user is existing or not
+        $or: [{ username }, { email }]
+    })
 
-const userRegistration = asyncHandler(async(req, res)=>
-{
-        const {email, username, fullname,password} = req.body; //get user Details
-        
-        if([fullname,email,username,password].some((field)=>field?.trim()==="")){  //user details not empty
-            throw new ApiError(400, "All filed fill mendetory")
+    if (userExist) {
+        throw new ApiError(409, "User alredy Exist", userExist)
+    }
+
+    const profilepicLoaclPath = await req.files?.profilepic[0]?.path;
+    console.log(profilepicLoaclPath);
+    
+    if (!profilepicLoaclPath) {
+        throw new ApiError(400, "Avatar file is reqired")
+    }
+
+    const profile = await uploadPhotoOnCloudinary(profilepicLoaclPath)
+
+    const coverpicLocalpath = await req.files?.coverpic[0]?.path;
+    if (!coverpicLocalpath) {
+        throw new ApiError(400, "cover photo is reqired")
+    }
+    const cover = await uploadPhotoOnCloudinary(coverpicLocalpath) 
+
+    const userData = await User.create(
+        {
+            fullname,
+            username,
+            email,
+            password,
+            profilepic : profile.url || "",
+            coverpic : cover.url || ""
         }
+    )
 
-        const userExist = await User.findOne({      //check user is existing or not
-            $or : [{username}, {email}]
-        })
+    const checkUser = await User.findById(userData._id).select(-password -refreshToken )
+    if(!checkUser){
+        throw new ApiError(500, "User Creation failed due to while register wrong Entry in the field")
+    }
 
-        if(userExist){
-            throw new ApiError(409, "User alredy Exist", userExist)
-        }
-
-        const profilepicLoaclPath =await req.files?.profilepic[0]?.path;
-        if(!profilepicLoaclPath){
-            throw new ApiError(400, "Avatar file is reqired")
-        }
-
-        const profile = await uploadPhotoOnCloudinary(profilepicLoaclPath)
-        
-        const coverpicLocalpath = await req.files?.coverpic[0]?.path;
-        if(!coverpicLocalpath) {
-            throw new ApiError(400, "cover photo is reqired")
-        }
-        const cover = await uploadPhotoOnCloudinary(coverpicLocalpath)
-        
-        // if (email ==="" || username ==="" || fullname ==="" || password===""){
-        //         throw new ApiError(400, "All field filled mendetory")
-        // }
-
-   
-
-// res.status(200).json({
-//         message : "Print firts news"
-//     })   
+    return res.status(201).json(
+        new ApiResponse(200, userData, "Created User Successfully" )    )
 })
 
 
-export {userRegistration}
+export { userRegistration }
